33950ee6ec5cad8c303a7ac9ac5c87d045b6da0b
braj011 <bhagyaraj01@gmail.com>
2018-07-17 16:29:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist;
+  delete playlist[artist];   // alt way to . 
   return playlist; 
 }
 
